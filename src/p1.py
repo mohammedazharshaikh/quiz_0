9704c427f9c28eeac006d903e0ce1c336d839f00
mohammedazharshaikh <68691119+mohammedazharshaikh@users.noreply.github.com>
2024-06-05 17:02:35 -0500

    a2

---
+++ src/p1.py
@@ -0,0 +1,7 @@
+
+
+import csv
+with open('q0c.csv', newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    for row in spamreader:
+        print(', '.join(row))
