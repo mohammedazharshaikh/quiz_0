cb501a5d3ee19731de4619ea0f005a8c85b46a13
mohammedazharshaikh <68691119+mohammedazharshaikh@users.noreply.github.com>
2024-06-05 19:48:27 -0500

    a6

--- src/p1.py
+++ src/p1.py
@@ -38,27 +38,18 @@ file_path = 'src/q0c.csv'
 df = pd.read_csv(file_path)
 
 def fetch_row_by_column_value(df, column_name, value):
-    # Filter the DataFrame based on the column and value
     filtered_row = df[df[column_name] == value]
-    
-    # Check if the result is not empty
     if filtered_row.empty:
         return None, df
-    
-    # Retrieve the first matching row (assuming unique values)
     row = filtered_row.iloc[0]
-    
-    # Get the rest of the columns' values for the row
     rest_of_columns = row.drop(labels=[column_name])
-    
     return rest_of_columns, df.drop(filtered_row.index)
 
-# Example usage
-column_name = 'name'  # Change this to the column to search
-value = 'cat'  # Change this to the value to search for
+column_name = 'Name'  
+value = 'cat' 
 result, remaining_df = fetch_row_by_column_value(df, column_name, value)
 
-print("Fetched Row (excluding specified column):")
+print("Fetched Row :")
 print(result)
 
 print("\nRemaining DataFrame:")
